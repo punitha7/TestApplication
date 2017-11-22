d025ed7d62be6089306d50b7b3e30fad3381c0c5
Jesse Pospisil <posmicanomaly@gmail.com>
2017-11-21 21:53:37 -0500

    Add prototype for CSVFile and CSVFileLoader for moving the logic out of Application class.

---
+++ org/punitha7/csvtools/CSVFile.java
@@ -0,0 +1,33 @@
+package org.punitha7.csvtools;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Representation of a loaded CSV file
+ * 
+ * @author Jesse Pospisil <posmicanomaly@gmail.com>
+ *
+ */
+public class CSVFile {
+	private ArrayList<String> rows;
+	
+	/**
+	 * Build a CSVFile from a File object
+	 * @param file File object of a CSV file
+	 */
+	public CSVFile(File file) {
+		/**
+		 * Implement adding the rows to the "rows" variable.
+		 */
+	}
+	
+	@Override
+	public String toString() {
+		String result = "";
+		/**
+		 * Implement a string representation of the CSV file that can be printed to the console.
+		 */
+		return result;
+	}
+}
